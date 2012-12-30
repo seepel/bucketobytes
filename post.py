74d0dec4851cf56f39fd7afbf947b4bf7c4bf499
Sean Lynch <sean@seanplynch.com>
2012-12-29 18:57:51 -0800

    Move Stream watcher to respond.py. Add post.py to randomly post

--- post.py
+++ post.py
@@ -12,6 +12,20 @@ parser.add_option('--access_token_key', dest='access_token_key')
 parser.add_option('--access_token_secret', dest='access_token_secret')
 (options, args) = parser.parse_args()
 
+from twython import Twython, TwythonError
+from optparse import OptionParser
+import os
+import random
+import pprint
+import time
+
+parser = OptionParser()
+parser.add_option('--consumer_key', dest='consumer_key')
+parser.add_option('--consumer_secret', dest='consumer_secret')
+parser.add_option('--access_token_key', dest='access_token_key')
+parser.add_option('--access_token_secret', dest='access_token_secret')
+(options, args) = parser.parse_args()
+
 if options.consumer_key == None:
   print "No consumer key"
   exit()
@@ -35,13 +49,6 @@ api = Twython(app_key=options.consumer_key,
               oauth_token=options.access_token_key,
               oauth_token_secret=options.access_token_secret)
 
-rates = api.getRateLimitStatus()
-remaining_hits = rates['remaining_hits']
-rate_reset = rates['reset_time_in_seconds']
-print 'Rate limit: ' + str(remaining_hits)
-
-current_user = api.verifyCredentials()
-
 fortunes = open('fortunes').read().split('\n%\n')
 if len(fortunes) == 0:
   exit()
@@ -50,71 +57,9 @@ for fortune in fortunes:
   if len(fortune) > 140:
     fortunes.remove(fortune)
 
-def followed(follow):
-  source_id = follow['source']['id_str']
-  target_id = follow['target']['id_str']
-  new_id = ""
-  if source_id != current_user['id_str']:
-    new_id = source_id
-  elif target_id != current_user['id_str']:
-    new_id = target_id
-    
-  print 'FRIEND: ' + new_id
-  api.createFriendship(user_id=new_id)
+while True:
+  fortune = random.choice(fortunes)
+  api.updateStatus(status=fortune)
+  print fortune
+  time.sleep(random.randrange(2700,4500,1))
 
-def cc(status):
-  api.reTweet(id=status['id_str'])
-  remaining_hits = remaining_hits-1
-
-def reply(status):
-  user = status['user']['id_str']
-  max_length = 140 - len(user)-2
-  content = random.choice(fortunes)
-  count = 0
-  while len(content) > max_length:
-    if count > 1000:
-      break
-    content = random.choice(fortunes)
-  content = '@' + status['user']['screen_name'] + ' ' + content
-  print content
-  api.updateStatus(status=content, in_reply_to_status_id=status['id_str'])
-  remaining_hits = remaining_hits-1
-
-def mentioned(status):
-  if '#cc' in status['text']:
-    cc(status)
-  else:
-    reply(status)
-
-
-def on_results(results):
-  global rate_reset
-  global remaining_hits
-  pp.pprint(results)
-  if remaining_hits <= 0:
-    print "hit my limit!"
-    return
-  if time.gmtime() > rate_reset:
-    rates = api.getRateLimitStatus()
-    remaining_hits = rates['remaining_hits']
-    rate_reset = rates['reset_time_in_seconds']
-    print 'Rate limit: ' + str(remaining_hits)
-  try:
-    if results.has_key('event'):
-      if results['event'] == 'follow':
-        followed(results)
-    elif results.has_key('entities'):
-      if results['user']['id_str'] == current_user['id_str']:
-        return
-      if results['entities'].has_key('user_mentions'):
-        for user_mention in results['entities']['user_mentions']:
-          if user_mention['id_str'] == current_user['id_str']:
-            mentioned(results)
-            break
-  except TwythonError:
-    api.updateStatus(status='@seepel I encountered an error!')
-
-try:
-  api.stream({ 'endpoint' : 'https://userstream.twitter.com/1.1/user.json' }, on_results)
-except TwythonError:
-  api.updateStatus(status='@seepel I have crashed!')
